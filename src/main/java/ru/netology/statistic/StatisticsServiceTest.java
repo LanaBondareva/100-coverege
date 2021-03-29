4af904a0062675ad8681404f1e93c8568399c54d
Svetlana Bondareva <b.s.a94@mail.ru>
2021-03-29 10:37:57 +0400

    changed test

--- src/main/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/main/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -8,7 +8,7 @@ class StatisticsServiceTest {
 
   @Test
   void findMax() {
-    ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+    StatisticsService service = new StatisticsService();
 
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
